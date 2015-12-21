81fe219a9dc9cba447ebeb9b4ddc0a9ba5bb44ab
ntorrent <ntorrent@expedia.com>
2015-12-21 17:15:10 +0100

    Adding the loging feature

--- azure-primer.js
+++ azure-primer.js
@@ -2,7 +2,6 @@
 
 var program = require('commander');
 var azure = require('azure-cli');
-var prompt = require('prompt');
 var exec = require('child-process-promise').exec;
 var jsonfile = require('jsonfile');
 
@@ -17,7 +16,7 @@ function createAzureResource(templateUrl) {
 
 function waitingDeploymentToFinish() {
     console.log('Waiting deployment to finish');
-    var schedule = setInterval(function () { 
+    var schedule = setInterval(function () {
         var cmd = 'azure group deployment show ' + resourceName;
         
         exec(cmd).then(function (result) {
@@ -28,20 +27,32 @@ function waitingDeploymentToFinish() {
             }
         });
     }, 5000);
-
+    
     return result;
 };
 
 function switchToArm() {
     console.log('-- Switching to arm mode --')
     return exec('azure config mode arm');
-}
+};
 
-var test = function () { 
+var test = function () {
     console.log('test ok');
-}
+};
 
-function createParams(name) {
+var login = function (userName, password) {
+    var cmd = 'azure login -u ' + userName + ' -p ' + password;
+    
+    return exec(cmd);
+};
+
+var selectSubscription = function (name) {
+    var cmd = 'azure account set "' + name + '"';
+    
+    return exec(cmd);
+};
+
+function createParams(name, repoUrl) {
     var file = 'params.json';
     var obj = {
         "$schema": "http://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#",
@@ -57,7 +68,7 @@ function createParams(name) {
                 "value": "Standard"
             },
             "repoURL": {
-                "value": "https://github.com/soft-nt/deployazurenode"
+                "value": repoUrl
             },
             "workerSize": {
                 "value": "0"
@@ -68,38 +79,65 @@ function createParams(name) {
     jsonfile.writeFile(file, obj, function (err) { });
 }
 
-var createWebSite = function (name) {
+var createWebSite = function (name, repoUrl) {
     console.log('--- Creating a web site called ' + name + ' ---');
     
     switchToArm().then(function () {
-        createParams(name);
-
+        createParams(name, repoUrl);
+        
         // Creating a resource based on web site template
         createAzureResource('https://raw.githubusercontent.com/soft-nt/AzurePrimer/master/ResourceTemplates/WebApp/WebApp.json').then(function (result) {
             console.log('Resource creation started');
             console.log('Result: ' + result.stdout);
-
+            
             waitingDeploymentToFinish();
-        }).fail(function(err) {
+        }).fail(function (err) {
             if (err.indexOf("result is not defined") < 0) {
-                console.error('ERROR: ', err);    
+                console.error('ERROR: ', err);
             }
         });
     });
 };
 
+
 // Exported functions
 exports.test = test;
 exports.createWebSite = createWebSite;
+exports.login = login;
+exports.selectSubscription = selectSubscription;
 
 // Creacting the commands
 program
   .version('0.0.1')
-  .command('createWebApp <name>')
+  .command('createWebApp <name> <repoUrl>')
   .alias('cwa')
-  .description('Create a web app on using Azure Primer')
+  .description('Create a web app using Azure Primer')
+  .action(function (name, repoUrl) {
+    createWebSite(name, repoUrl);
+});
+
+program
+  .version('0.0.1')
+  .command('login <userName> <password>')
+  .description('Login on azure')
+  .action(function (userName, password) {
+    login(userName, password).then(function (result) { 
+        console.log(result.stdout);
+    }).fail(function (err) { 
+        console.log(err);
+    });
+});
+
+program
+  .version('0.0.1')
+  .command('selectSubscription <name>')
+  .description('Select subscription on azure')
   .action(function (name) {
-    createWebSite(name);
+    selectSubscription(name).then(function (result) {
+        console.log(result.stdout);
+    }).fail(function (err) {
+        console.log(err);
+    });
 });
 
 program.parse(process.argv);
